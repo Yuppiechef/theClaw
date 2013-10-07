66222f985e7099ce7b8b2becc12c9d95a34f49f2
Yuppiechef Tech <tech@yuppiechef.com>
2013-10-07 23:02:18 +0000

    i2c stuff

--- src/theclaw/core.clj
+++ src/theclaw/core.clj
@@ -2,21 +2,29 @@
   (:use [theclaw.util])
   (:require
    [clojure.tools.nrepl.server :as nrepl]
-   [theclaw.queues :as mq])
+   [theclaw.queues :as mq]
+   [theclaw.i2c :as i2c])
   (:gen-class))
+
+(def last-pos (atom [-1 0 0]))
 
 (defn claw-move [_ {:keys [x y sessionid] :as msg}]
   (info "Position" msg)
-  (mq/publish "claw.moved" {:lastx 10 :lasty 20 :sequence 1}))
+  (i2c/write-command i2c/d :move (Short/parseShort x) (Short/parseShort y))
+  (reset! last-pos [sessionid x y])
+  (mq/publish "claw.moved" {:lastx x :lasty y :time (/ (.getTime (java.util.Date.)) 1000)}))
 
 (defn claw-grab [_ {:keys [sessionid] :as msg}]
   (info "Drop" msg)
   ;;after grabbing
-  (mq/publish "claw.grabbed" {:prizeid 12345 :x 10 :y 20})
-  (mq/publish "claw.calibrate" {})
+  (let [[sessionid x y] @last-pos]
+    (i2c/write-command i2c/d :grab)
+    (mq/publish "claw.grabbed" {:prizeid 12345 :lastx 10 :lasty 20 :sessionid sessionid})
+    (mq/publish "claw.calibrate" {}))
   )
 
 (defn claw-calibrate [_ msg]
+  (i2c/write-command :calibrate)
   (info "Calibrate" msg)
   {:msg "hi"})
 
---
+++ src/theclaw/i2c.clj
@@ -0,0 +1,37 @@
+(ns theclaw.i2c
+  (:import
+   [com.pi4j.io.i2c I2CBus I2CDevice I2CFactory]
+   [java.nio ByteBuffer]))
+
+(def bus (atom nil))
+(def d (atom nil))
+
+(defn device [id]
+  (.getDevice @bus id))
+
+(defn setup-bus []
+  (reset! bus (I2CFactory/getInstance I2CBus/BUS_1))
+  (reset! d (device 0x04)))
+
+(defn write-string [device string]
+  (let [s (str string "\n")]
+    (.write device (.getBytes s) 0 (count (.getBytes s)))))
+
+(def commands
+  {:zero 0
+   :calibrate 1
+   :move 2
+   :grab 3
+   :speed 4})
+
+(defn write-command [device command-key & [arg1 arg2 arg3 arg4]]
+  (let [b (doto (ByteBuffer/allocate 6)
+            (.putShort (get commands command-key))
+            (.putShort (short (or arg1 0)))
+            (.putShort (short (or arg2 0))))
+        a (.array b)]
+    (.write device a 0 (count a))))
+
+
+
+
